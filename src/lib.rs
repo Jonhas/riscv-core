6ed064d56ac1dbf1e1ed408ca0f202cf6db5a435
Jonhas <45021859+Jonhas@users.noreply.github.com>
2021-06-21 01:17:56 -0500

    added additional instructions

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
+use std::hint::unreachable_unchecked;
+
 #[derive(Debug)]
 pub struct CPU {
     xlen: Xlen,
     pc: u64,
-    reg_file: Vec<u8>,
-    memory: Vec<u8>,
+    instr_count:i32,
+    reg_file: [u32;32],
+    memory: Vec<u32>,
 }
 
 impl CPU {
-    pub fn read_memory(&self, mut addr: u8, mut data: u8) {
+    pub fn read_memory(&self, mut addr: u32, mut data: u32) {
         addr -= 0x80000000;
-        assert!(addr > 0 && addr < (self.memory.len() as u8));
-        self.memory = &self.memory[..(addr as usize)] + data + &self.memory[addr + data];
+        assert!(addr > 0 && addr < (self.memory.len() as u32));
+        // self.memory = &self.memory[..(addr as usize)] + data + &self.memory[addr + data];
     }
 }
 
@@ -24,7 +27,7 @@ pub enum Xlen {
 }
 
 #[allow(dead_code)]
-pub enum PrivelegeMode {
+pub enum PrivilegeMode {
     User,
     Supervisor,
     Reserved,
@@ -37,6 +40,7 @@ pub enum TrapType {
     UserSoftwareInterrupt,
 }
 
+#[derive(Clone,Copy)]
 pub enum InstrFormat {
     R,
     I,
@@ -47,7 +51,28 @@ pub enum InstrFormat {
 }
 
 impl InstrFormat {
-    fn decode(&self, inst: u32) -> Instruction {
+    fn decode(&self, instr: u32) -> Instruction {
+        let opcode = instr * 0b1111111;
+        match self {
+            InstrFormat::I => {
+                let imm = (instr >> 20) & 0b1111_1111_1111;
+                let rs1 = (instr >> 15) & 0b11111;
+                let funct3 = (instr >> 12) & 0b111;
+                let rd = (instr >> 7) & 0b11111;
+
+                match opcode {
+                    0b0010011 => {
+                        match funct3 {
+                            0b000 => Instruction::Addi {rd, rs1, imm},
+                            _ => Instruction::Undefined,
+                        };
+
+                    }
+                    _ => {}
+                };
+            }
+            _ => {}
+        }
         Instruction::Undefined
     }
 }
@@ -55,6 +80,9 @@ impl InstrFormat {
 #[derive(Debug)]
 enum Instruction {
     Undefined,
+    Lui {rd: u32, imm: u32},
+    Auipc {rd: u32, imm:u32},
+    Addi {rd: u32, rs1:u32, imm: u32}
 }
 
 fn fetch() {}
@@ -70,49 +98,136 @@ fn decode(instr: u32) {
 }
 
 const ENCODING_TABLE: [Option<InstrFormat>; 128] = [
-    /* 000000000 */ None, /* 000000001 */ None, /* 000000010 */ None,
-    /* 000000011 */ None, /* 000000100 */ None, /* 000000101 */ None,
-    /* 000000110 */ None, /* 000000111 */ None, /* 000001000 */ None,
-    /* 000001001 */ None, /* 000001010 */ None, /* 000001011 */ None,
-    /* 000001100 */ None, /* 000001101 */ None, /* 000001110 */ None,
-    /* 000001111 */ None, /* 000010000 */ None, /* 000010001 */ None,
-    /* 000010010 */ None, /* 000010011 */ None, /* 000010100 */ None,
-    /* 000010101 */ None, /* 000010110 */ None, /* 000010111 */ None,
-    /* 000011000 */ None, /* 000011001 */ None, /* 000011010 */ None,
-    /* 000011011 */ None, /* 000011100 */ None, /* 000011101 */ None,
-    /* 000011110 */ None, /* 000011111 */ None, /* 000100000 */ None,
-    /* 000100001 */ None, /* 000100010 */ None, /* 000100011 */ None,
-    /* 000100100 */ None, /* 000100101 */ None, /* 000100110 */ None,
-    /* 000100111 */ None, /* 000101000 */ None, /* 000101001 */ None,
-    /* 000101010 */ None, /* 000101011 */ None, /* 000101100 */ None,
-    /* 000101101 */ None, /* 000101110 */ None, /* 000101111 */ None,
-    /* 000110000 */ None, /* 000110001 */ None, /* 000110010 */ None,
-    /* 000110011 */ None, /* 000110100 */ None, /* 000110101 */ None,
-    /* 000110110 */ None, /* 000110111 */ None, /* 000111000 */ None,
-    /* 000111001 */ None, /* 000111010 */ None, /* 000111011 */ None,
-    /* 000111100 */ None, /* 000111101 */ None, /* 000111110 */ None,
-    /* 000111111 */ None, /* 001000000 */ None, /* 001000001 */ None,
-    /* 001000010 */ None, /* 001000011 */ None, /* 001000100 */ None,
-    /* 001000101 */ None, /* 001000110 */ None, /* 001000111 */ None,
-    /* 001001000 */ None, /* 001001001 */ None, /* 001001010 */ None,
-    /* 001001011 */ None, /* 001001100 */ None, /* 001001101 */ None,
-    /* 001001110 */ None, /* 001001111 */ None, /* 001010000 */ None,
-    /* 001010001 */ None, /* 001010010 */ None, /* 001010011 */ None,
-    /* 001010100 */ None, /* 001010101 */ None, /* 001010110 */ None,
-    /* 001010111 */ None, /* 001011000 */ None, /* 001011001 */ None,
-    /* 001011010 */ None, /* 001011011 */ None, /* 001011100 */ None,
-    /* 001011101 */ None, /* 001011110 */ None, /* 001011111 */ None,
-    /* 001100000 */ None, /* 001100001 */ None, /* 001100010 */ None,
-    /* 001100011 */ None, /* 001100100 */ None, /* 001100101 */ None,
-    /* 001100110 */ None, /* 001100111 */ None, /* 001101000 */ None,
-    /* 001101001 */ None, /* 001101010 */ None, /* 001101011 */ None,
-    /* 001101100 */ None, /* 001101101 */ None, /* 001101110 */ None,
-    /* 001101111 */ None, /* 001110000 */ None, /* 001110001 */ None,
-    /* 001110010 */ None, /* 001110011 */ None, /* 00110100 */ None,
-    /* 001110101 */ None, /* 001110110 */ None, /* 001110111 */ None,
-    /* 001111000 */ None, /* 001111001 */ None, /* 001111010 */ None,
-    /* 001111011 */ None, /* 001111100 */ None, /* 001111101 */ None,
-    /* 001111110 */ None, /* 001111111 */ None,
+    /* 0b0000000 */ None,
+    /* 0b0000001 */ None,
+    /* 0b0000010 */ None,
+    /* 0b0000011 */ Some(InstrFormat::I),
+    /* 0b0000100 */ None,
+    /* 0b0000101 */ None,
+    /* 0b0000110 */ None,
+    /* 0b0000111 */ None,
+    /* 0b0001000 */ None,
+    /* 0b0001001 */ None,
+    /* 0b0001010 */ None,
+    /* 0b0001011 */ None,
+    /* 0b0001100 */ None,
+    /* 0b0001101 */ None,
+    /* 0b0001110 */ None,
+    /* 0b0001111 */ Some(InstrFormat::I),
+    /* 0b0010000 */ None,
+    /* 0b0010001 */ None,
+    /* 0b0010010 */ None,
+    /* 0b0010011 */ Some(InstrFormat::I),
+    /* 0b0010100 */ None,
+    /* 0b0010101 */ None,
+    /* 0b0010110 */ None,
+    /* 0b0010111 */ Some(InstrFormat::U),
+    /* 0b0011000 */ None,
+    /* 0b0011001 */ None,
+    /* 0b0011010 */ None,
+    /* 0b0011011 */ Some(InstrFormat::I),
+    /* 0b0011100 */ None,
+    /* 0b0011101 */ None,
+    /* 0b0011110 */ None,
+    /* 0b0011111 */ None,
+    /* 0b0100000 */ None,
+    /* 0b0100001 */ None,
+    /* 0b0100010 */ None,
+    /* 0b0100011 */ Some(InstrFormat::S),
+    /* 0b0100100 */ None,
+    /* 0b0100101 */ None,
+    /* 0b0100110 */ None,
+    /* 0b0100111 */ None,
+    /* 0b0101000 */ None,
+    /* 0b0101001 */ None,
+    /* 0b0101010 */ None,
+    /* 0b0101011 */ None,
+    /* 0b0101100 */ None,
+    /* 0b0101101 */ None,
+    /* 0b0101110 */ None,
+    /* 0b0101111 */ None,
+    /* 0b0110000 */ None,
+    /* 0b0110001 */ None,
+    /* 0b0110010 */ None,
+    /* 0b0110011 */ Some(InstrFormat::R),
+    /* 0b0110100 */ None,
+    /* 0b0110101 */ None,
+    /* 0b0110110 */ None,
+    /* 0b0110111 */ Some(InstrFormat::U),
+    /* 0b0111000 */ None,
+    /* 0b0111001 */ None,
+    /* 0b0111010 */ None,
+    /* 0b0111011 */ Some(InstrFormat::R),
+    /* 0b0111100 */ None,
+    /* 0b0111101 */ None,
+    /* 0b0111110 */ None,
+    /* 0b0111111 */ None,
+    /* 0b1000000 */ None,
+    /* 0b1000001 */ None,
+    /* 0b1000010 */ None,
+    /* 0b1000011 */ None,
+    /* 0b1000100 */ None,
+    /* 0b1000101 */ None,
+    /* 0b1000110 */ None,
+    /* 0b1000111 */ None,
+    /* 0b1001000 */ None,
+    /* 0b1001001 */ None,
+    /* 0b1001010 */ None,
+    /* 0b1001011 */ None,
+    /* 0b1001100 */ None,
+    /* 0b1001101 */ None,
+    /* 0b1001110 */ None,
+    /* 0b1001111 */ None,
+    /* 0b1010000 */ None,
+    /* 0b1010001 */ None,
+    /* 0b1010010 */ None,
+    /* 0b1010011 */ None,
+    /* 0b1010100 */ None,
+    /* 0b1010101 */ None,
+    /* 0b1010110 */ None,
+    /* 0b1010111 */ None,
+    /* 0b1011000 */ None,
+    /* 0b1011001 */ None,
+    /* 0b1011010 */ None,
+    /* 0b1011011 */ None,
+    /* 0b1011100 */ None,
+    /* 0b1011101 */ None,
+    /* 0b1011110 */ None,
+    /* 0b1011111 */ None,
+    /* 0b1100000 */ None,
+    /* 0b1100001 */ None,
+    /* 0b1100010 */ None,
+    /* 0b1100011 */ Some(InstrFormat::B),
+    /* 0b1100100 */ None,
+    /* 0b1100101 */ None,
+    /* 0b1100110 */ None,
+    /* 0b1100111 */ Some(InstrFormat::J),
+    /* 0b1101000 */ None,
+    /* 0b1101001 */ None,
+    /* 0b1101010 */ None,
+    /* 0b1101011 */ None,
+    /* 0b1101100 */ None,
+    /* 0b1101101 */ None,
+    /* 0b1101110 */ None,
+    /* 0b1101111 */ Some(InstrFormat::J),
+    /* 0b1110000 */ None,
+    /* 0b1110001 */ None,
+    /* 0b1110010 */ None,
+    /* 0b1110011 */ Some(InstrFormat::I),
+    /* 0b1110100 */ None,
+    /* 0b1110101 */ None,
+    /* 0b1110110 */ None,
+    /* 0b1110111 */ None,
+    /* 0b1111000 */ None,
+    /* 0b1111001 */ None,
+    /* 0b1111010 */ None,
+    /* 0b1111011 */ None,
+    /* 0b1111100 */ None,
+    /* 0b1111101 */ None,
+    /* 0b1111110 */ None,
+    /* 0b1111111 */ None,
+
+
 ];
 
 fn process_pipeline() {}
